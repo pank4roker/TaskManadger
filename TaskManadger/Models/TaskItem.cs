aba48d13556369ae9c0727ba2a5f59fcfa68a4a8
Markow Kirill <kirillmarkor@gmail.com>
2025-09-26 18:59:12 +0300

    create model

---
+++ TaskManadger/Models/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManadger.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }              // ID задачи
+    public string Title { get; set; }        // Заголовок
+    public string Description { get; set; }  // Описание
+    public bool IsCompleted { get; set; }    // Статус выполнения
+    public DateTime CreatedAt { get; set; }  // Дата создания
+}
